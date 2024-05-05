4f471f17d2aba7d82cca223522408f030b2b7087
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 14:28:51 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,24 @@
+variable "group" {
+  type = string
+  default = "My Resource Group"
+}
 
+variable "account" {
+  type = string
+  default = "My Storage Account"
+}
+
+variable "plan" {
+  type = string
+  default = "My Service Plan"
+}
+
+variable "app" {
+  type = string
+  default = "My Linux App"
+}
+
+variable "function" {
+  type = string
+  default = "My Function"
+}
