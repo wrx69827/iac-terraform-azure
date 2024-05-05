59b1a12e1c6bc270cc15d6e7c493a8337f64df69
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 14:56:05 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,19 @@
+variable "group" {
+  type = string
+  default = "My Resource Group"
+}
 
+variable "account" {
+  type = string
+  default = "My Storage Account"
+}
+
+variable "my_area" {
+  type = string
+  default = "Europe"
+}
+
+variable "tier" {
+  type = string
+  default = "Standard"
+}
