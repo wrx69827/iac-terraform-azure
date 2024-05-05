9c7945991a52e97318db75e9b4cb5b928344d78d
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 15:04:32 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -22,3 +22,28 @@ variable "replication_type" {
   type = string
   default = "GRS"
 }
+
+variable "container_name" {
+  type = string
+  default = "vhds"
+}
+
+variable "storage_name" {
+  type = string
+  default = "private"
+}
+
+variable "storage_content" {
+  type = string
+  default = "my-awesome-content.zip"
+}
+
+variable "type" {
+  type = string
+  default = "Block"
+}
+
+variable "source" {
+  type = string
+  default = "some-local-file.zip"
+}
