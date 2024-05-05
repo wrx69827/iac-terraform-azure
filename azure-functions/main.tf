ab233f02c5b00c247f92cab0b77078639c081a65
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 14:28:53 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
+  name     = var.group
   location = "Middle Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "my_storage_account"
+  name                     = var.account
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "my_service_plan"
+  name                = var.plan
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   os_type             = "Windows"
@@ -24,7 +24,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_linux_function_app" "example" {
-  name                = "my_linux_function_app"
+  name                = var.app
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
@@ -35,8 +35,8 @@ resource "azurerm_linux_function_app" "example" {
   site_config {}
 }
 
-resource "azurerm_function_app" "example" {
-  name                       = "my_function_app"
+resource "azurerm_function_app_function" "example" {
+  name                       = var.function
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   app_service_plan_id        = azurerm_app_service_plan.example.id
