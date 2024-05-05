0ca3cbeedba23b46212fa506962d34215defffe7
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 15:04:28 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -14,3 +14,17 @@ resource "azurerm_storage_account" "example" {
     environment = "staging"
   }
 }
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = var.storage_name
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = var.storage_content
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = var.type
+  source                 = var.source
+}
