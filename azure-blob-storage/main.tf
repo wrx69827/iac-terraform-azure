202f8c77ecca53191db9a1ed393b0afa3961bc7b
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 14:55:02 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,16 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.group
+  location = var.my_area
+}
 
+resource "azurerm_storage_account" "example" {
+  name                     = var.account
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.tier
+  account_replication_type = var.replication_type
+
+  tags = {
+    environment = "staging"
+  }
+}
