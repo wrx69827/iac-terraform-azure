1f3eea446af1e79c4533eb287ac52fc2967fdd03
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 14:50:14 +0200

    Update outputs.tf

--- azure-functions/outputs.tf
+++ azure-functions/outputs.tf
@@ -1,4 +1,3 @@
-resource "azurerm_function_app_function_id" "example" {
-  name = "id"
-  function_app_id = azurerm_function_app_function.example.id
+output "app_function_id" {
+  value = azurerm_function_app_function.example.id
 }
