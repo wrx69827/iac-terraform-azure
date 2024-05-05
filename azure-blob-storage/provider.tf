e07d85c509109d6dfad97fe5b4634ad4c027d678
wrx69827 <152792473+wrx69827@users.noreply.github.com>
2024-05-05 15:05:38 +0200

    Update provider.tf

--- azure-blob-storage/provider.tf
+++ azure-blob-storage/provider.tf
@@ -1,1 +1,17 @@
-
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+  skip_provider_registration = true 
+  features {}
+}
